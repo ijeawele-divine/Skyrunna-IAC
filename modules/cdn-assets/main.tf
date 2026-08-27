221a7fac3e2fe549dc74e7ba948179c88d30a36b
ijeawele-divine <nkwochaijeawele@gmail.com>
2026-08-27 22:55:26 +0100

    Comment out S3 bucket policy resource in CDN assets module

--- modules/cdn-assets/main.tf
+++ modules/cdn-assets/main.tf
@@ -90,6 +90,7 @@ data "aws_iam_policy_document" "assets" {
   }
 }
 
+#resource
 resource "aws_s3_bucket_policy" "assets" {
   bucket = aws_s3_bucket.assets.id
   policy = data.aws_iam_policy_document.assets.json
